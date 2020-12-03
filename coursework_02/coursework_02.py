bfa8b958b802e67d2c7facaa3419adc494d6c9ca
Weicheng Ao <weicheng.ao@student.manchester.ac.uk>
2020-12-03 03:16:06 +0000

    finalise the UI design

--- coursework_02/coursework_02.py
+++ coursework_02/coursework_02.py
@@ -24,7 +24,7 @@ def startgame():
         starttimer = 0
 
         balls = []
-        
+
         if gamefromsave == 1:
             file = open("posxl.txt")
             file1 = open("posxr.txt")
@@ -53,7 +53,7 @@ def startgame():
         killer=canvas.create_oval(killerxy, fill="orange")
         if gamefromsave == 1:
             file4 = open("name.txt")
-            player = canvas.create_text( 1280/2-200 , 10 , fill="white" , font="Times 20 italic bold", text="PLAYER: " + file4.read())
+            player = canvas.create_text( 1280/2-200 , 30 , fill="white" , font="Times 20 italic bold", text="PLAYER: " + file4.read())
             file4.close()
 
             canvas.config(bg="black")
@@ -61,18 +61,18 @@ def startgame():
             file5 = open("score.txt")
             score = int(file5.read())
             txt = "Score:" + str(score)
-            scoreText = canvas.create_text( 1280/2+50 , 10 , fill="white" , font="Times 20 italic bold", text=txt)
+            scoreText = canvas.create_text( 1280/2+50 , 30 , fill="white" , font="Times 20 italic bold", text=txt)
             file5.close()
 
         else:
-            player = canvas.create_text( 1280/2-200 , 10 , fill="white" , font="Times 20 italic bold", text="PLAYER: " + playername.get())
+            player = canvas.create_text( 1280/2-200 , 30 , fill="white" , font="Times 20 italic bold", text="PLAYER: " + playername.get())
             canvas.config(bg="black")
             score = 0
             txt = "Score:" + str(score)
-            scoreText = canvas.create_text( 1280/2+50 , 10 , fill="white" , font="Times 20 italic bold", text=txt)
-        
+            scoreText = canvas.create_text( 1280/2+50 , 30 , fill="white" , font="Times 20 italic bold", text=txt)
 
-
+        canvas.create_text( 1280/2-80 , 700 , fill="yellow" , font="Times 20 italic bold", text="<p> to pause, <k> to save, <b> is the Boss Key, <c> to cheat")
+        canvas.create_text( 1280/2-80 , 670 , fill="blue" , font="Times 20 italic bold", text="when GreenBall touched the Middle of the OrangeBall, Game Over.")
 
 
 
@@ -83,41 +83,34 @@ def startgame():
 
         canvas.bind("<p>", pause)
 
-        canvas.bind("<h>", hide)
+        canvas.bind("<b>", hide)
 
-        
 
         canvas.bind("<"+left.get()+">", leftKey)
         canvas.bind("<"+right.get()+">", rightKey)
         canvas.bind("<"+up.get()+">", upKey)
         canvas.bind("<"+down.get()+">", downKey)
 
-        # canvas.bind("<MouseWheel>",zoomer)
         canvas.focus_set()
         direction = "right"
-        # return canvas
+
         placeFood()
         canvas.bind("<KeyPress>", call_back)
         if starttimer == 0:
             countdown = 30
             starttimer = 1
-            timermsg = canvas.create_text( 1280/2 , 36 , fill="white" , font="Times 20 italic bold", text="Time Remaining for Next Update: "+str(countdown))
+            timermsg = canvas.create_text( 1280/2 , 50 , fill="white" , font="Times 20 italic bold", text="Time Remaining for Next Update: "+str(countdown))
             timer()
-        
-        # print(foodcoord)
 
         canvas.after(90, moveBall)
         canvas.after(1000, movekiller)
-        # print(str(balls[0]))
-        # moveBall()
-        # sleep(5)
-        # canvas.destroy()
+
 
 def pause(event):
     global pause, pausemsg
     if pause == 0:
         pause = 1
-        pausemsg = canvas.create_text( 1280/2 , 720/2 , fill="white" , font="Times 20 italic bold", text="The Game Is Paused, Press P to return." )
+        pausemsg = canvas.create_text( 1280/2 , 720/2 , fill="white" , font="Times 20 italic bold", text="The Game Is Paused, Press <p> to return." )
     else:
         canvas.delete(pausemsg)
         pause = 0
@@ -133,7 +126,7 @@ def movekiller():
     else:
         if countdown != 0:
             if xrg-(xl+xr)/2 <= (xr-xl)/2 and xrg-(xl+xr)/2 >= -(xr-xl)/2 and yrg-(yl+yr)/2 <= (yr-yl)/2 and yrg-(yl+yr)/2 >= -(yr-yl)/2:
-            # if (xl <= xrg and yr >= yrg and yl <= ylg) or (yr >= ylg and xl <= xlg and xr >= xrg) or (xr >= xlg and yr >= yrg and yl <= ylg) or (yrg >= yl and xl <= xlg and xr >= xrg):
+
                 if not os.path.isfile('total.txt'):
                     newfile = open('total.txt','w')
                     newfile.write('1')
@@ -141,12 +134,12 @@ def movekiller():
 
                     newfile = open('1name.txt','w')
                     newfile.write('Anonymous')
-                    newfile.close()                    
+                    newfile.close()
 
                     newfile = open('1score.txt','w')
                     newfile.write('50')
                     newfile.close()
-                
+
                 if os.path.isfile('total.txt'):
                     file = open('total.txt')
                     total = int(file.read())
@@ -162,24 +155,8 @@ def movekiller():
                     newfile = open(str(total+1)+'name.txt','w')
                     newfile.write(playername.get())
                     newfile.close()
-                # if firsttime == 1:
-                #     leaderfile.write(open('leader.ini','w'))
-                #     leaderfile.add_section('total')
-                #     leaderfile.set("total","total","1")
-                #     leaderfile.set("total","1name","")
-                #     leaderfile.set("total","1score","0")
-                #     firsttime = 0
-                # else:
-                #     leaderfile.write(open('leader.ini','w'))
-
 
                 end = 1
-
-
-                # leadernum = leaderfile.get('total', 'total')
-                # leaderfile.set('total', 'total', str(int(leadernum)+1))
-                # leaderfile.set('total', str(int(leadernum)+1)+'name', playername.get())
-                # leaderfile.set('total', str(int(leadernum)+1)+'score', str(score))
 
                 start = 0
 
@@ -250,7 +227,6 @@ def timer():
             canvas.after(90, timer)
         else:
             if countdown > 0:
-                # boom()
                 for i in range (len(foodcoordx)):
                     if foodcoordx[i]-(xl+xr)/2 <= (xr-xl)/2 and foodcoordx[i]-(xl+xr)/2 >= -(xr-xl)/2 and foodcoordy[i]-(yl+yr)/2 <= (yr-yl)/2 and foodcoordy[i]-(yl+yr)/2 >= -(yr-yl)/2:
                         break
@@ -271,11 +247,9 @@ def timer():
                            pball=canvas.create_oval(xy, fill="green")
                            break
 
-
                 canvas.itemconfigure(timermsg, text="Time Remaining for Next Update: "+str(countdown))
                 countdown -= 1
             else:
-                # boomdir()
                 directiong = rand(0, 3)
                 countdown = 30
                 movspeed += 15
@@ -287,7 +261,6 @@ def leaderboard():
         end = 0
         movspeed = 50
         gamefromsave = 0
-
 
     if not os.path.isfile('total.txt'):
         newfile = open('total.txt','w')
@@ -301,7 +274,6 @@ def leaderboard():
         newfile = open('1score.txt','w')
         newfile.write('50')
         newfile.close()
-
 
     leaders = Canvas(mainwindow, width=1280, height=720)
     leaders.pack()
@@ -328,8 +300,8 @@ def leaderboard():
         leaders.create_text(1280/2 , 350 , fill="red" , font="Times 20 italic bold", text="1ST Position: "+filehighest.read()+"  Score: "+filehighest2.read())
 
         filehighest.close()
-        filehighest2.close()       
-        
+        filehighest2.close()
+
         secondpos = 1
         for i in range(totalnum):
             filehighest = open(str(secondpos)+'score.txt')
@@ -341,14 +313,13 @@ def leaderboard():
             filescore.close()
             filehighest.close()
 
-        
         filehighest = open(str(secondpos)+'name.txt')
         filehighest2 = open(str(secondpos)+'score.txt')
 
-        leaders.create_text(1280/2 , 450 , fill="orange" , font="Times 20 italic bold", text="2ND Position: "+filehighest.read()+"  Score: "+filehighest2.read()) 
+        leaders.create_text(1280/2 , 450 , fill="orange" , font="Times 20 italic bold", text="2ND Position: "+filehighest.read()+"  Score: "+filehighest2.read())
 
         filehighest.close()
-        filehighest2.close() 
+        filehighest2.close()
 
         thipos = 1
         for i in range(totalnum):
@@ -361,14 +332,13 @@ def leaderboard():
             filescore.close()
             filehighest.close()
 
-
         filehighest = open(str(thipos)+'name.txt')
         filehighest2 = open(str(thipos)+'score.txt')
 
-        leaders.create_text(1280/2 , 550 , fill="purple" , font="Times 20 italic bold", text="3RD Position: "+filehighest.read()+"  Score: "+filehighest2.read()) 
+        leaders.create_text(1280/2 , 550 , fill="purple" , font="Times 20 italic bold", text="3RD Position: "+filehighest.read()+"  Score: "+filehighest2.read())
 
         filehighest.close()
-        filehighest2.close() 
+        filehighest2.close()
 
         Button(leaders, text='Back', font=('Arial', 15), width=10, height=1, command=destroyleader).place(x=1280/2, y=600, anchor='nw')
 
@@ -382,8 +352,6 @@ def moveBall():
      canvas.after(90, moveBall)
     else:
         canvas.pack()
-        # foodlenbef = len(foodcoordx)
-        # fooddel = []
         for i in range (len(foodcoordx)):
          if foodcoordx[i]-(xl+xr)/2 <= (xr-xl)/2 and foodcoordx[i]-(xl+xr)/2 >= -(xr-xl)/2 and foodcoordy[i]-(yl+yr)/2 <= (yr-yl)/2 and foodcoordy[i]-(yl+yr)/2 >= -(yr-yl)/2:
             canvas.delete(food[i])
@@ -399,16 +367,13 @@ def moveBall():
             growth = 0
 
             xy = (xl -growth,yl -growth,xr +growth,yr +growth)
-            # xy = (1280/2 -growth,720/2 -growth,1280/2 +growth,720/2 +growth)
 
             xl=xy[0]
             yl=xy[1]
             xr=xy[2]
             yr=xy[3]
-            # print(xy[0])
             pball=canvas.create_oval(xy, fill="green")
             balls.append(pball)
-            # canvas.scale(pball, (xr-xl)/2, (yr-yl)/2, 1.1, 1.1)
             break
 
         if len(foodcoordx) < 10:
@@ -435,21 +400,7 @@ def moveBall():
              yl += 30
              yr += 30
 
-        #     if overlapping(sHeadPos, canvas.coords(snake[i])):
-        #         gameOver = True
-        #         canvas.create_text(width/2,height/2,fill="white",font="Times 20 italic bold", text="Game Over!")
-        # for i in range(1,len(balls)):
-        #  positions.append(canvas.coords(balls[i]))
-        # for i in range(len(balls)-1):
-        #  canvas.coords(balls[i+1],positions[i][0], positions[i][1],positions[i][2],positions[i][3])
-
         canvas.after(90, moveBall)
-
-        # for i in range(1,len(snake)):
-        #     positions.append(canvas.coords(snake[i]))
-        # for i in range(len(snake)-1):
-        #     canvas.coords(snake[i+1],positions[i][0], positions[i][1],positions[i][2],positions[i][3])
-        # if 'gameOver' not in locals():
 
 def hide(event):
     global hider, pause, canvashade
@@ -458,27 +409,22 @@ def hide(event):
         pause=1
         canvashade = Canvas(canvas, width=1280, height=720, bg='white')
         canvashade.pack()
-        shademsg = canvashade.create_text( 1280/2 , 720/2 , fill="black" , font="Times 20 italic bold", text="Welcome to Python, You Pressed a BOSS KEY! Press again to restore." )
-        # mainwindow.withdraw()
-        # bosskeydis = canvas.create_image(img=bosskey)
+        shademsg = canvashade.create_text( 1280/2 , 720/2 , fill="black" , font="Times 20 italic bold", text="Welcome to Python, You Pressed a BOSS KEY! Press <b> again to restore." )
+
 
         if os.path.isfile('python.png'):
             photo = PhotoImage(file='python.png')
-            img_label = Label(canvashade, img=photo)
+            img_label = Label(canvashade, image=photo)
+            # img_label.pack()
     else:
         hider=0
         pause=0
         canvashade.destroy()
 
-        
-
-        # canvas.delete(bosskeydis)
-
 def call_back(event):
     global call_back, pause, pausemsg, score, movspeed, xl, xr, yl, yr
-    # print(event.keysym)
-    if event.keysym == "p":
 
+    if event.keysym == "p":
         if pause == 0:
             pause = 1
             pausemsg = canvas.create_text( 1280/2 , 720/2 , fill="white" , font="Times 20 italic bold", text="The Game Is Paused, Press <p> to return." )
@@ -488,12 +434,6 @@ def call_back(event):
     elif event.keysym == "k":
         if pause == 0:
             pause = 1
-            # gamesaver.write(open('config.ini','w'))
-            # newfile.write('< Statistics Summary >\n'+'the total number of words :')
-            # gamesaver.add_section('db')
-            # gamesaver.set("db", "db_pass", "xgmtest")
-            # gamesaver.close()
-            # print(gamesaver.get("db", "db_pass"))
 
             newfile=open('name.txt','w')
             newfile.write(playername.get())
@@ -505,20 +445,19 @@ def call_back(event):
 
             newfile=open('posxl.txt','w')
             newfile.write(str(xl))
-            newfile.close()            
-            
+            newfile.close()
+
             newfile=open('posxr.txt','w')
             newfile.write(str(xr))
-            newfile.close() 
- 
+            newfile.close()
+
             newfile=open('posyl.txt','w')
             newfile.write(str(yl))
-            newfile.close() 
+            newfile.close()
 
             newfile=open('posyr.txt','w')
             newfile.write(str(yr))
-            newfile.close() 
-
+            newfile.close()
 
             pausemsg = canvas.create_text( 1280/2 , 720/2 , fill="white" , font="Times 20 italic bold", text="Game saved successfully, Press <k> to return." )
         else:
@@ -544,9 +483,9 @@ def placeFood():
     	foodY = rand(0,700)
     	foodcoordx.append(foodX)
     	foodcoordy.append(foodY)
- 
+
     	canvas.move(food[i], foodX, foodY)
-    	
+
 def leftKey(event):
     global direction
     direction = "left"
@@ -571,14 +510,13 @@ def load():
     else:
         messagebox.showinfo(title='Ooops!', message='You have not saved a game ! Please play a little bit the game and press <k> to save.')
 
-
 mainwindow = Tk()
 
 mainwindow.title("Battle Of Balls (adapted)")
 mainwindow.geometry('1280x720')
 
-Label(mainwindow,text='Battle Of Balls (adapted)', font=('Arial Bold', 20)).place(x=500, y=200, anchor='nw')
-Label(mainwindow,text='Please Type Your Name :', font=('Arial Bold', 15)).place(x=350, y=250, anchor='nw')
+Label(mainwindow,text='Battle Of Balls (adapted)', font=('Arial Bold', 20)).place(x=500, y=50, anchor='nw')
+Label(mainwindow,text='Please Type Your Name :', font=('Arial Bold', 15)).place(x=350, y=100, anchor='nw')
 
 playername = StringVar()
 playername.set("Harvey")
@@ -614,23 +552,33 @@ end = 0
 gamesaver = configparser.ConfigParser()
 leaderfile = configparser.ConfigParser()
 
-e = Entry(mainwindow, show=None, font=('Arial', 14), textvariable=playername).place(x=600, y=250, anchor='nw')
-# playername.set('you hit me')
-Button(mainwindow, text='Start !', font=('Arial', 15), width=10, height=1, command=startgame).place(x=600, y=350, anchor='nw')
-# User control
-Button(mainwindow, text='Reload a Game from local', font=('Arial', 15), width=30, height=1, command=load).place(x=800, y=350, anchor='nw')
-Label(mainwindow,text='User Settings(Customary Control Keys):', font=('Arial Bold', 15)).place(x=500, y=720/2+50, anchor='nw')
-Label(mainwindow,text='UpKey :', font=('Arial Bold', 15)).place(x=450, y=440, anchor='nw')
-upk=Entry(mainwindow, show=None, font=('Arial', 14), textvariable=up).place(x=550, y=440, anchor='nw')
-Label(mainwindow,text='DownKey:', font=('Arial Bold', 15)).place(x=450, y=470, anchor='nw')
-downk=Entry(mainwindow, show=None, font=('Arial', 14), textvariable=down).place(x=570, y=470, anchor='nw')
-Label(mainwindow,text='LeftKey:', font=('Arial Bold', 15)).place(x=450, y=500, anchor='nw')
-leftk=Entry(mainwindow, show=None, font=('Arial', 14), textvariable=left).place(x=570, y=500, anchor='nw')
-Label(mainwindow,text='RightKey:', font=('Arial Bold', 15)).place(x=450, y=530, anchor='nw')
-rightk=Entry(mainwindow, show=None, font=('Arial', 14), textvariable=right).place(x=570, y=530, anchor='nw')
-Label(mainwindow,text='! Notice: The keys <Left> <Right> <Up> <Down> are kept unchanged intentationally just in case the Customary Keys do not work.', font=('Arial Bold', 15)).place(x=40, y=570, anchor='nw')
+e = Entry(mainwindow, show=None, font=('Arial', 14), textvariable=playername).place(x=650, y=100, anchor='nw')
+
+Button(mainwindow, text='Start !', font=('Arial', 15), width=10, height=1, command=startgame).place(x=350, y=150, anchor='nw')
+Button(mainwindow, text='Reload a Game from local', font=('Arial', 15), width=30, height=1, command=load).place(x=550, y=150, anchor='nw')
+
+# User control (Customary Control keys)
+
+Label(mainwindow,text='User Settings(Customary Control Keys):', font=('Arial Bold', 15)).place(x=500, y=200, anchor='nw')
+Label(mainwindow,text='UpKey :', font=('Arial Bold', 15)).place(x=450, y=230, anchor='nw')
+upk=Entry(mainwindow, show=None, font=('Arial', 14), textvariable=up).place(x=550, y=230, anchor='nw')
+Label(mainwindow,text='DownKey:', font=('Arial Bold', 15)).place(x=450, y=260, anchor='nw')
+downk=Entry(mainwindow, show=None, font=('Arial', 14), textvariable=down).place(x=570, y=260, anchor='nw')
+Label(mainwindow,text='LeftKey:', font=('Arial Bold', 15)).place(x=450, y=290, anchor='nw')
+leftk=Entry(mainwindow, show=None, font=('Arial', 14), textvariable=left).place(x=570, y=290, anchor='nw')
+Label(mainwindow,text='RightKey:', font=('Arial Bold', 15)).place(x=450, y=320, anchor='nw')
+rightk=Entry(mainwindow, show=None, font=('Arial', 14), textvariable=right).place(x=570, y=320, anchor='nw')
+Label(mainwindow,text='Notice: The four direction keys <Left> <Right> <Up> <Down> are reserved unchanged.', font=('Arial Bold', 15)).place(x=50, y=350, anchor='nw')
+Label(mainwindow,text='Guidence(During the game): 1.Press <p> to pause, 2.<b> is the Boss Key, ', font=('Arial Bold', 15)).place(x=40, y=380, anchor='nw')
+Label(mainwindow,text='3.Press <k> to save the game: You can reload it from the main manu once you stored.', font=('Arial Bold', 15)).place(x=50, y=410, anchor='nw')
+Label(mainwindow,text='4.Press <c> to cheat: Each time you pressed the key <c>, your score will be added by 50', font=('Arial Bold', 15)).place(x=50, y=440, anchor='nw')
+Label(mainwindow,text=' and the speed of the KillerBall will go down.', font=('Arial Bold', 15)).place(x=50, y=470, anchor='nw')
+Label(mainwindow,text='5.Game Over: Once the ball you control(the green one)', font=('Arial Bold', 15)).place(x=50, y=500, anchor='nw')
+Label(mainwindow,text='touches the middle of the KillerBall(orange one), the game is over and your score will be saved.', font=('Arial Bold', 15)).place(x=50, y=530, anchor='nw')
+Label(mainwindow,text='6.LeaderBoard: Once the game finished, the leaderboard window will come out. press back to return', font=('Arial Bold', 15)).place(x=50, y=560, anchor='nw')
+
+Label(mainwindow,text='Have a nice day :)', font=('Arial Bold', 20)).place(x=1280/2-150, y=620, anchor='nw')
+
 
 foodcoord=[]
 mainwindow.mainloop()
-
-
